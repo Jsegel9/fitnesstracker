005931f194b9b7b93699b8408e6a2ead0b239862
Jsegel <segeljosh@gmail.com>
2020-02-13 01:12:29 -0600

    adding db

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public")); 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./public/api"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port http://localhost:${PORT}`);
+  });
+  
